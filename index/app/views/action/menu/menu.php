877504085702de471e7216f554ad4e551a4de6b5
liukailcl <tuoxuanshe@163.com>
2014-09-08 17:37:53 +0800

    头要炸

--- index/app/views/action/menu/menu.php
+++ index/app/views/action/menu/menu.php
@@ -1,102 +1,87 @@
 <!DOCTYPE html>
-<html lang="zh-CN" class="uk-text-center uk-height-1-1">
+<html lang="zh-CN" >
   <head>
     <meta charset='utf-8' />
     <link rel="stylesheet" href="<?php echo  base_url('public/css/Action.menu.css');?>" />
+	<link rel="stylesheet" href="<?php echo  base_url('public/css/addons/uikit.addons.min.css');?>" />
+
 	<script src="<?php echo base_url('public/js/Action.menu.js') ?>"></script>
+	<script src="<?php echo base_url('public/js/addons/notify.min.js') ?>"></script>
+	<script src="<?php echo base_url('public/js/addons/sticky.min.js') ?>"></script>
   </head>
   
   
-  <body class="uk-container-center uk-height-1-1 uk-width-1-1 ">
+  <body >
   
-  
- 	<nav class="uk-width-1-1">
-	   
-	   <a  class="uk-button uk-icon uk-icon-navicon uk-icon-large" data-uk-offcanvas="{target:'#menu-options',bgclose:true}"></a>
-
-	   <a  id="printM-target" class="uk-button uk-icon uk-icon-check-square-o uk-icon-large uk-float-right" data-uk-modal="{target:'#print-modal',bgclose:false}">  </a>  
-	
+ 	<nav class="uk-width-1" data-uk-sticky>
+ 		<div class="uk-width-1 uk-grid uk-grid-preserve">
+	   <button  class="uk-width-1-2 uk-button uk-icon uk-icon-navicon uk-icon-large uk-text-left " data-uk-offcanvas="{target:'#menu-optclass',bgclose:true}"></button>
+	   <button  class="uk-width-1-2 uk-button uk-icon uk-icon-check-square-o uk-icon-large uk-text-right" ></button>  
+ 		</div>
  	</nav>
 
     
 
   
-  
-  
-  <ul class="uk-list uk-list-line uk-list-space uk-text-bold uk-text-middle uk-text-center uk-height-1-1" data-uk-margin>
- 
+
+<div class="uk-width-1 uk-container uk-container-center">  
+  <ul class="uk-width-1 uk-list uk-list-space uk-grid uk-grid-preserve uk-container uk-container-center">
+	  
   <?php foreach($list as $li): ?>  		 
-      <li class="uk-width-1-1 uk-grid uk-grid-preserve menu-item" value="<?php echo $li['dishid'] ?>">
-      	<img class="uk-thumbnail uk-width-1-6" src="<?php echo base_url('/public/img/test.jpg') ?>">
-      	<e class="uk-width-1-3 uk-text-middle export"><?php echo '<val class="cm">'.$li['cm'].'</val>' ?></e>
-       <b class="uk-badge uk-badge-success uk-badge-notification uk-float-right">
-       <h4> <span class="uk-icon-jpy"><?php echo '<val class="jg">'.$li['jg'].'</val>' ?></span></h4>
-       </b>
-       </li>
+      <li class="menu-item uk-width-1 uk-grid uk-grid-preserve " value="<?php echo $li['dishid'] ?>">
+      	
+      	<span type="button" class="uk-float-left clearItem uk-badge uk-badge-danger uk-badge-notification uk-icon uk-icon-minus uk-icon-medium uk-button" style="display : none"></span>
+      		  	
+	  	<p class="uk-width-1-2 " ><val class="cm" ><?php echo $li['cm'] ?></val></p>
+	    
+	    <p class="uk-width-1-2" style="display:none"><val class="sl uk-badge uk-badge-notification">已选1份<val></p>
+	  	
+	  	<val class="uk-width-2-10 jg uk-badge uk-badge-success uk-badge-notification uk-icon-jpy uk-float-right"><?php echo $li['jg'] ?></val>
+	  	
+	   </li>
       
       <?php endforeach ?>
       
   </ul>
   
-  	<div id="menu-options" class="uk-offcanvas uk-container-center uk-text-bold uk-text-middle uk-text-center">
-    <div class="uk-offcanvas-bar">
-        <ul class="uk-nav uk-nav-offcanvas " data-uk-nav data-uk-margin>
+  	<div id="menu-optclass" class="uk-offcanvas uk-text-bold">
+    <div class="uk-offcanvas-bar uk-text-center " >
+    	<div class="uk-panel">
+    		 <h3 class="uk-panel-title">Menu</h3>
+    		 <hr />
+        	<ul class="uk-nav uk-nav-side">
 	        <li>
 	          <a href="#">火锅/主菜</a>
 	        </li>
 	        
-	        <li>
+	        <li >
 	          <a href="#">凉菜/素菜</a>
 	        </li>
 	        
-	        <li>
+	        <li >
 	          <a href="#">荤菜/热菜</a>
 	        </li>
 	        
-	        <li>
+	        <li >
 	          <a href="#">小炒/特色</a>
 	        </li>
 	        
-	        <li>
+	        <li >
 	          <a href="#">主食/汤羹</a>
 	        </li>
 	        
-	        <li>
+	        <li >
 	          <a href="#">其他/新品</a>
 	        </li>
 	        
 	        
         </ul>
+    	</div>
     </div>
   </div>
   
   
-  	<form id="print-modal" class="uk-modal uk-modal-large" >
-    	<div class="uk-modal-dialog uk-modal-dialog-frameless">
-       		 <a  class="uk-modal-close uk-close uk-close-alt"> </a>
-	   		 
-	   		 <table class="uk-table uk-text-bold uk-text-center	uk-table-hover uk-table-condensed">
-	   		 
-	   		  
-	   		 	<tr >
-	   		 
-	   		 	
-	   		 	<td class="uk-width-1-3 "><input class="uk-button uk-width-1" type="button" value="菜品" /></td>
-	   		 	<td class="uk-width-1-3"><input class="uk-button uk-width-1" type="button" value="数量" /></td>
-	   		 	<td class="uk-width-1-3"><input class="uk-button uk-width-1" type="button" value="价格" /></td>
-	   		 	 
-	   		 	
-	   		 	</tr>
-	   		 	
-	   		 	
-	   		 	
-	   		 	
-	   		 
-	   		 </table>
-	   		 
-		</div>
-	</form>
-  
+  	  
   </body>
   
   
--- public/js/Action.menu.js
+++ public/js/Action.menu.js
@@ -1,111 +1,51 @@
 
-$('Document').ready(function(){
-			var TouchObj =  document.getElementsByTagName('val');
-			console.log(TouchObj);
-			
-			var Marra = new Array;
-			
-			var menu_data ={ demo : '1' } ;
-										
-		
-	   	
-	 $("li.menu-item").on('click',menu_data,function(e){
+$('Document').ready(function(){	
 	 
+	var mdata ;
+	
+	$("li.menu-item").on('click',mdata,function(e){
 
-	 		if($(this).hasClass('uk-panel-box-success')){
-		 	var sl = parseInt($(this).find('val.sl').text());  
-		 	e.data.value.sl = sl+1;	
-		 	$(this).find('val.sl').text(sl+1);	
-		 			
-		  		
+	 		if($(e.currentTarget).hasClass('uk-li-success')){
+		 	
+		 		$(e.currentTarget).find('val.sl').text(parseInt($(e.currentTarget).find('val.sl').text())+1); 		 	
+	 		
 	 		}else{
 	 		
-	 			e.data.Domli = $(e.currentTarget).clone(true);	
-			   $(e.currentTarget).addClass('uk-panel-box-success').append("<span class='uk-badge uk-width-1-3'>已选<val class='sl'>1</val>份</span>").prepend("<a  class='uk-float-left uk-close uk-close-alt'></a>").find('img.uk-thumbnail').hide('fast');
-			    e.data.value = {dish : e.currentTarget.value , sl : 1};
-			  
-						 			  
-				
+	 		
+		 		$(this).data($(e.currentTarget).clone(true)) ;
+			
+		 			$(e.currentTarget).children(':hidden').show('fast');
+		
+			 	
 				}	
 				
 			
-				//$('uk-close-alt').delegate(
-				
-				if($(e.target).hasClass('uk-close-alt')){
-				 
-				 	$(e.data.Domli).fadeIn('fast','linear').replaceAll($(e.currentTarget));
-				
-					}
-					
-		
-					//}	
-					
-				 
-				
-				 Marra.push(e.data.value);
-				//console.log(Marra);		
-				
-				
-		});
-				
-				
-				
-				
-				
-				
-				
-				
-				/*var sl = parseInt($(this).find('val.sl').text());  	
-				$(this).find('val.sl').text(sl+1);		
-						
-			$(this).addClass('uk-panel-box-success').prepend("<span class='uk-badge uk-width-1-3'>已选<val class='sl'>1</val>份</span><a id='box-succ-close' class='uk-float-right uk-close uk-close-alt'></a>");
-			
-			*/
-			
-			 
-	
-	
-	
-	
-			
-	  $("a[href='#print-modal']").on('click',function(e){
-	  		$(this).css('width','200px');
-		  
-		  	e.stopPropagation();
-		  	
-		  	var modal = $.UIkit.modal("#print-modal");
-		  	var menuD = $('div.uk-panel-box-success').find('li');
-		  	var table = $('table.uk-table');
-		  
-				
-		  	if ( modal.isActive() ) {
-		  		
-		  		//table.replaceAll('table.uk-table');
-			  	modal.hide();
-			  	
-			  	
-			  	  }else{
-			  	  
-				modal.show();
-								
-				menu_data.items =  new Object ;
-				
-				menuD.each(function(n){
+							
+				if($(e.target).hasClass('clearItem')){
 				 	
-				var valv = this.value; 
-				var vals = $.trim($(this).find('val.sl').text());
-				var valj = $.trim($(this).find('val.jg').text());
-				menu_data.items[valv] = vals+'x'+valj;
-	
-			});	
-								
-			}	
+				$($(this).data()).show().replaceAll(e.currentTarget);
 				
-			
-			
+				
+		       }
 		
 		});
+				
+				
+			
+	 
+
+
+
+	
+
 		
+
+
+		
+			
+			
+		
+				
 		
 });
 
