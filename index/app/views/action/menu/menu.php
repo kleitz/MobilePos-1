11d6e43f9d70fb6015daa3b5a6d769264feace9a
liukailcl <tuoxuanshe@163.com>
2014-10-05 03:38:37 +0800

    reset 3fce777fdb9eef4762e4325a4099c6fc08e30b8a

--- index/app/views/action/menu/menu.php
+++ index/app/views/action/menu/menu.php
@@ -1,76 +1,55 @@
 <!doctype html>
-<html lang="en" >
+<html lang="zh-cn" >
   <head>
     <meta charset='utf-8' />
+    <link rel="stylesheet" href="<?php echo  base_url('public/css/action.menu.css');?>" />
 	<link rel="stylesheet" href="<?php echo  base_url('public/css/addons/uikit.addons.min.css');?>" />
-	<link rel="stylesheet" href="<?php echo  base_url('public/css/action.menu.css');?>" />
+
 	<script src="<?php echo base_url('public/js/action.menu.js') ?>"></script>
 	<script src="<?php echo base_url('public/js/addons/notify.min.js') ?>"></script>
 	<script src="<?php echo base_url('public/js/addons/sticky.min.js') ?>"></script>
   </head>
   
   
-  <body class="uk-height-mini-1-1 uk-text-center">
-  <section id="selected" class="uk-nbfc uk-container-max uk-vertical-align" data-uk-scrollspy="{cls:'uk-animation-slide-top'}" data-uk-sticky>
-                    <div class="uk-vertical-align-bottom">
-					
-					<a  type="button" class="uk-text-left uk-align-left uk-icon-th-large uk-vertical-align-bottom uk-icon-large  " data-uk-offcanvas="{target:'#menu-optclass'}" > </a> 
-					<a  id="targetm" type="button" class="uk-text-right uk-align-right uk-vertical-align-bottom uk-icon-list-alt uk-icon-large " > </a>
-               
-                    </div>
-             <hr />
-    </section>
+  <body >
+  
+ 	<nav class="uk-width-1 uk-animation-slide-top">
+ 		<div class="uk-width-1 uk-grid uk-grid-preserve uk-animation-slide-top">
+	   <button  class="uk-width-1-2 uk-button uk-icon uk-icon-navicon uk-icon-large uk-text-left" data-uk-offcanvas="{target:'#menu-optclass'}"></button>
+	   <button id="modalbtn" class="uk-width-1-2 uk-button uk-icon uk-icon-check-square-o uk-icon-large uk-text-right print-modaltarget "></button>  
+ 		</div>
+ 	</nav>
+
     
+
   
-        <div class="uk-nbfc-alt uk-heading uk-width-1 uk-container-max">
-		 	 
-		 	 	<nav id="headnav" class="uk-width-1">
-	 			<ul class="uk-icon">
-          
-				
-        <blockquote class="uk-text-left"> 
 
-										Hey Menu
-
-								<img class="uk-thumbnail-kay uk-text-center uk-margin" src="<?php echo base_url('./favicon.ico') ?>" / >
-                <img class="uk-thumbnail-kay-lb uk-text-center uk-margin" src="<?php echo base_url('./public/img/banner-1.png') ?>" / >
-        </blockquote>
-					
-	 			
-				</ul>
-	 	 	 </nav>
-	 	 	
-               </div>
-
-
-<form id="untreated-form"  class="uk-container-max uk-width-1 uk-vertical-align" data-uk-scrollspy="{cls:'uk-animation-fade'}">
-  
-		 
+ 	<div class="uk-width-1 uk-container uk-container-center">  
+  <ul class="uk-width-1 uk-list uk-list-space uk-grid uk-grid-preserve uk-container uk-container-center uk-animation-scale-up">
 	  
-  <?php foreach($list as $li): ?> 
-
-<fieldset class="item uk-container-max uk-text-justify uk-margin"  form="order-manu" name="ordermenu">
-<input class="uk-float-right uk-text-center" id="<?php echo $li['dishid'] ?>" type="checkbox" name="item" >
-		<div class="uk-hidden">
-		<a class="uk-close uk-close-alt"></a>
-        <a class="sl uk-badge uk-text-right uk-float-right"><var class="uk-text-primary uk-icon-cutlery">1</var></a>
-		</div>	
-		<a class="uk-text-center uk-align-center"><?php echo $li['cm'] ?></a>				  
-		<a class="uk-text-center uk-icon-jpy  uk-badge-price uk-align-center" ><var><?php echo $li['jg'] ?></var></a>
-
-</fieldset>	
+  <?php foreach($list as $li): ?>  
+  
+  
+  	 <span class="clearitem uk-icon uk-icon-minus-circle uk-icon-medium" style="display:none"></span>
+  	 <span class="uk-icon uk-icon-cutlery uk-float-right" style="display:none"><val class="sl"><b class="uk-text-primary">1</b></val></span>		 
+      <li class="menu-item uk-width-1 uk-grid uk-grid-preserve uk-text-bottom" value="<?php echo $li['dishid'] ?>">  		  	
+	  	<val class="cm uk-width-2-3 uk-text-left"  ><?php echo $li['cm'] ?></val></p>
+	  	<val class="uk-width-1-3 uk-width-medium-1-10 jg uk-icon-jpy uk-badge"><?php echo $li['jg'] ?></val>	
+	   </li>
+  
       
       <?php endforeach ?>
-</form>
-
-		
+      
+  </ul>
   
-		<div id="menu-optclass" class="uk-offcanvas uk-text-bold">
+</div>
+  
+  	<div id="menu-optclass" class="uk-offcanvas uk-text-bold">
     <div class="uk-offcanvas-bar uk-text-center " >
     	<div class="uk-panel">
-    		 <h3 class="uk-panel-title">Menu</h3>
+    		 <h3 class="uk-panel-title">menu</h3>
     		 <hr />
-        	<ul class="uk-nav uk-nav-side uk-list uk-list-striped">
+        	<ul class="uk-nav uk-nav-side">
 	        <li>
 	          <a href="#">火锅/主菜</a>
 	        </li>
@@ -103,35 +82,19 @@
   	
   	
   	
-  	<div id="menu-modal" class="uk-modal uk-container-max uk-container-center">
-  	  	
-  	  	
-  	  	<nav class="uk-text-center uk-container-max uk-animation-fade">
-
-	    <a  class=" uk-button  uk-width-1-2 uk-float-left uk-animation-silde-left uk-text-left" >
-						<i class="uk-width-1 uk-icon-times  uk-modal-close"></i></a>
-	  	    
-	    <a  class=" uk-button uk-width-1-2 uk-float-right uk-animation-silde-right uk-text-right uk-text-success">
-						<i class="uk-width-1 uk-icon-check-square-o uk-text-success"></i></a>
-	    
-        </nav>
-  	  	
-	    <div class="uk-modal-dialog uk-modal-dialog-large uk-modal-dialog-frameless" >
-	    
-	    
-	    
-	    <div id="print-form" name="order-menu" class="uk-form uk-container-max uk-animation-scale-down uk-overflow-container"> 	
-					 	
-        </div>
+  	
+  	<div id="menu-print-modal" class="uk-modal uk-container uk-container-center uk-width-1">
+	    <div class="uk-modal-dialog uk-modal-dialog-large uk-modal-dialog-frameless">
+	        <a class="uk-modal-close uk-close uk-close-alt"></a>
+	        <h2 class="uk-text-bold uk-text-center">清单预览</h2>
+	        <div class="uk-overflow-container">
+	        	<table class="uk-table uk-width-1 uk-container-center">
+	        		
 	        
-	       	        
+	        	</table>
+	        </div>
 		</div>
-		
-		
   	</div>  
-
-
-
   	  
   </body>
   
--- public/js/Action.menu.js
+++ public/js/Action.menu.js
@@ -1,69 +1,156 @@
-$(document).ready(function(e){
-    
+$('Document').ready(function(e){	
+ 
+		 $.CreateMenu = ({
+		 
+		    'result' : function(){ data =  $('.menu-item') ; return data; },
+		
+	 	   'isActive' : function(){ data =  $(this).is('tr') ; return data; } ,
+	 	    
+	 	   'addSl' : function(e){
+	 	  
+	 	  $(e.currentTarget).find('.sl').text(function(){
+			 var sl = parseInt($(this).text());
+			 		return sl+1;
+			 		});
+			 		
+			 		return e;
+			 		},
+	 		
+		 				 			
+		   'toggletl' : function(bool){
+		     
+		      var fadetr ;
+		   
+		   if(bool){
+		   
+		   	 $(this).wrap("<tr class='uk-li-success'></tr>").fadeOut('fast'); 
+		  
+		    
+		    }else{
+		    
+		   $(this).unwrap().fadeIn('fast');
+		
+		    	}
+		    	
+		 
+			
+			 
+		     
+		    }, 
+		    
+		    });
+		    
+	
+	
+	
+		 
+		$.CreateMenu.result().on('click',function(e){
+			var bool = $.CreateMenu.isActive();
+				
+			var fadetr = $($.CreateMenu.toggletl(bool));
+	
+			console.log(fadetr);			
+			
+			
+		
+		 		/*
+		 		$(e.currentTarget).triggerHandler('click',e);
+			  
+			  $('.uk-li-success').delegate('.clearitem','click',function(e){
+				
+				 			});
+			
+			
+			});
 
-    var suc = 'suc';     
-        
-  
-  //<form id="untreated-form"  class="uk-container-max uk-width-1 uk-vertical-align"
-  //<fieldset class="item uk-container-max uk-text-justify uk-margin customfield "  form="order-manu" name="menu">
+		
+		
+		$('#modalbtn').on('click',function(e){
+			
+			var modal = $.UIkit.modal("#menu-print-modal");
+			modal.options.bgclose = false ;
+			modal.show();
+			
+					
+		});
+		
+		
+		});
 
-  
-  //<form id="print-form" name="order-menu" class="uk-form uk-container-max uk-animation-scale-down uk-overflow-container"> 
+			*/
+			
+			
+	
 
-    function voli(e,suc,form,bool){
-          
-          
-            //判断当前冒泡元素是否有 suc 这个类 
-           //  如果没有的话,添加 suc 类, 插入到 form
-           //   如果有的话, 移除 suc 类 , 插入到 form
-            if(bool){
-            
-            return $(e.delegateTarget).appendTo(form).addClass(suc); 
-            
-            }                
-            return false;
-          };
+			 		
+		     		
+				/*>var table = $('table.uk-table');
+				
+					   
+					  if($(e.target).is('button.uk-icon-check-square-o')){
+							
+							$('li.uk-li-success').removeClass('uk-animation-reverse').clone(true).appendTo(table);
+							
+						
+						}
+						
+												
+					
+					if($(e.target).is('span')){
+				  			
+				        $(e.target).closest('li.uk-li-success').removeClass('uk-li-success').remove();
+				        //triggerHandler('click').addClass('uk-animation-reverse');
+					  			
+				  		
+				  		}
 
-    
-     $('fieldset').on('click',function(e){
-     var menugroup = $('div#untreated');
-     
-            if($(e.currentTarget).parent().is(menugroup)){
-             var  item  = voli(e,suc,'#print-form',true);  
-               $(item).find(':hidden').show('fast');         
-           }else{
-                voli(e,suc,'#untreated',false);  
-               
-           }
-        
-        
-        });
-        
-        
-      $('#targetm').on('click',function(e){
-          
-            var oder = $('fieldset.suc');
+					  
+					  
+					    modal.show();
+					
+					});
+					
+*/				
+			
+				
+				
+				
+			});
 
-                if ( oder.length === 0 ) {
-                    $.UIkit.notify({
-                        message : '没点任何菜,先选一样',
-                        status  : 'info',
-                        timeout : 3000,
-                        pos     : 'top-center'
-                    });
 
-                } else {
-                var modal = $.UIkit.modal("div#menu-print-modal");
-                    modal.show();
-                }
-         
-          
-      }); 
-        
-        
-        
-        
-        
-     
-               
-});
+
+});								
+				
+						
+		 
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
